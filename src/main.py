1f63de70449e968521fe45fa9c0551783d6504ea
dorogo <ill.shir@gmail.com>
2021-09-21 20:21:39 +0300

    добавил отключение output по аргументу

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 from bot import Bot
 import sys
+import os
+
+
+# Disable
+def block_print():
+    sys.stdout = open(os.devnull, 'w')
+
 
 if __name__ == '__main__':
     print("it's started!")
+    if 'blockPrint' in sys.argv:
+        block_print()
     # получаем токен из cfg файла
     with open('../conf/my.cfg', 'r') as cfg:
         token = cfg.read()
