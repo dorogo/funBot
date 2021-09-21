16b9034afe4f78a7ff8789835bdf6413b8eca8c7
dorogo <ill.shir@gmail.com>
2021-09-21 20:38:17 +0300

    fix print

--- src/main.py
+++ src/main.py
@@ -18,7 +18,6 @@ if __name__ == '__main__':
         if token is None:
             print("Cant find bot token")
             sys.exit()
-    print(f'token = {token}')
     bot = Bot()
     bot.init(token)
     bot.start()
